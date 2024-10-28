fb8eb9857b5a0404593022e49f78ab6dda963168
Raveen <raveenrv92@gmail.com>
2024-10-28 14:51:18 +0530

    Completed register Controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,48 @@
+const User = require("../model/user.model");
+const bcrypt = require("bcrypt");
+const generateTokenAndSetCookies = require("../utils/generateTokenAndSetCookies");
+
 const register = async (req, res) => {
-  res.send("Register User");
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      throw new Error("Required Username, Email, and Password");
+    }
+
+    const emailAlreadyExist = await User.findOne({ email });
+    const userAlreadyExist = await User.findOne({ username });
+
+    if (emailAlreadyExist) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Email Already Exist, Please login" });
+    }
+    if (userAlreadyExist) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Username Already Exist" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    generateTokenAndSetCookies(res, newUser._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully",
+      user: { ...newUser._doc, password: undefined },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 const login = async (req, res) => {
---
+++ backend/utils/generateTokenAndSetCookies.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const generateTokenAndSetCookies = (res, userId) => {
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+
+  return token;
+};
+
+module.exports = generateTokenAndSetCookies;
